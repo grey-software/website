aee862adece38c4e30f976507e69cf21f4de7cbc
mills <milindvishnoi@gmail.com>
2020-08-03 00:00:25 +0530

    Added 1x2 component and apprentice landing page

---
+++ components/OneCrossTwo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-if="! reverse" class="row">
+      <div class="col-md-8 col-12">
+        <h1 class="g-hero-heading">{{ title }}</h1>
+        <p class="g-hero-tag">{{ desc }}</p>
+      </div>
+      <div class="col-md-4 col-12">
+        <img class="g-hero-img" :src="imgSrc" />
+      </div>
+    </div>
+    <div v-else class="row">
+      <div class="col-md-4 col-12">
+        <img class="g-hero-img" :src="imgSrc" />
+      </div>
+      <div class="col-md-8 col-12 text-right">
+        <h1 class="g-hero-heading">{{ title }}</h1>
+        <p class="g-hero-tag">{{ desc }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imgSrc: String,
+    title: String,
+    desc: String,
+    reverse: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
---
+++ pages/apprentice/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <section class="container">
+      <div class="row mt-5 g-hero" align-v="center">
+        <div class="col-md-8">
+          <h1 class="g-hero-heading">Apprentice Program</h1>
+          <p
+            class="g-hero-tag"
+          >The apprentice program offers you the opportunity to learn software development by contributing to open source apps as an engineer, designer, or product manager with guidance and structure from a mentor.</p>
+        </div>
+        <div class="col-md-4 justify-content-center d-flex mt-3">
+          <img class="g-hero-img" src="@/assets/material-math.png" />
+        </div>
+      </div>
+    </section>
+    
+    <section class="container">
+      <OneCrossTwo reverse="true" title="Title" desc="hifehugyrfjbrujnhirfuburbu uhfeuihfoinf egfuibjsnifbe bufefbuene d" imgSrc="/projects/toonin/logo.svg" />
+    </section>
+  </div>
+</template>
+
+<script>
+import OneCrossTwo from '@/components/OneCrossTwo.vue'
+
+export default {
+  components: {
+    OneCrossTwo
+  }
+};
+</script>
+
+<style>
+</style>
