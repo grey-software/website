53d9e9535b4deb527d00cc5443ecff7dc0792d13
Osama Saleh <osama.saleh@mail.utoronto.ca>
2020-08-11 00:10:46 -0400

    Scraping tool completed to be leveraged for creating contribution cards

--- GithubScrape.js
+++ GithubScrape.js
@@ -6,7 +6,8 @@ const url = 'https://github.com/grey-software/grey.software/graphs/contributors'
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(url, { waitUntil: 'networkidle0' });
+
   await page.waitForSelector('#contributors', {
     visible: true,
   })
@@ -14,93 +15,68 @@ const url = 'https://github.com/grey-software/grey.software/graphs/contributors'
   const pageContent = await page.content()
 
   const $ = cheerio.load(pageContent);
+  
+  //Variables defining user contribution
+  const avatars = [];
+  const usernames = [];
+  const ranks = [];
+  const commits = [];
+  const additions = [];
+  const deletions = [];
+  const users = [];
+  const user = {avatar: "", rank: "", commits: "", additions: "", deletions: ""}
+
+  //Scraping for img link 
   $('.contrib-person').each(function (i, elem) {
-    console.log(elem)
+    avatars.push((elem.children[0]).children[0].children[0].children[0].attribs.src)
+    
   });
+
+  //Scraping for username
+  $('.contrib-person').each(function (i, elem) {
+    usernames.push((elem.children[0]).children[0].children[2].children[0].data);
+
+  });
+
+  //Scraping for rank
+  $('.contrib-person').each(function (i, elem) {
+    if((elem.children[0]).children[0].children[1].children[0] != null){
+        ranks.push((elem.children[0]).children[0].children[1].children[0].data);
+    }
+
+  });
+
+
+  //Scraping for commits, additions, and deletions 
+  $('.contrib-person').each(function (i, elem) {
+    commits.push((elem.children[0]).children[0].children[3].children[0].children[0].children[0].data);
+    additions.push((elem.children[0]).children[0].children[3].children[0].children[2].children[0].data);
+    deletions.push((elem.children[0]).children[0].children[3].children[0].children[4].children[0].data);
+    
+
+  });
+
+  var count = 0;
+  for(singleUser in usernames){
+      users.push({
+          avatar: avatars[count],
+          rank: ranks[count],
+          commits: commits[count],
+          username: singleUser,
+          additions: additions[count],
+          deletions: deletions[count]
+        
+        
+        });
+      count += 1;
+
+  }
+
+  console.log(users);
+
   await browser.close();
 })();
 
-
-// puppeteer
-//   .launch()
-//   .then(function(browser) {
-//     return browser.newPage();
-//   })
-//   .then(function(page) {
-//     return page.goto(url).then(function() {
-//       page.waitForSelector('.contrib-person', {
-//         visible: true,
-//       }).then(function() {  
-//         return page.content();
-//       })
-//     });
-//   })
-
-
-//   .then(function(html) {
-//     console.log(html);
-//     const $ = cheerio.load(html); 
-//     let contributors = [];
-//     $('.graphs').each(function(i, elem) {
-//         contributors[i] = $(this).text().trim();
-//         //console.log($(elem).text().trim());
-//     });
-//     console.log(contributors);       
-
-
-//   })
-
-
-
-//   .catch(function(err) {
-
-//     console.log(err);
-//   });
-
-
-
-
-
-
-// /*window.addEventListener('DOMContentLoaded', function() {
-
-//     axios.get('https://github.com/grey-software/grey.software/graphs/contributors')
-//         .then((response) => {
-//             if(response.status === 200) {
-//                 const $ = cheerio.load(response.data); 
-//                 let contributors = [];
-//                 $('.graphs').each(function(i, elem) {
-//                     contributors[i] = $(this).text().trim();
-//                     //console.log($(elem).text().trim());
-
-
-
-//                 });
-//                 console.log(contributors);
-//             }
-
-//     }, (error) => console.log(err) );   
-// });*/        
-
-
-// /*const casperjs = require("casperjs");
-// const casper = casperjs.create();
-
-
-// casper.start();
-// casper.open('https://github.com/grey-software/grey.software/graphs/contributors');
-// casper.then(function() {
-//     //Sanity check
-//     this.test.assertExists("#nav-check");
-//     var links = this.evaluate(function(){
-//         var results = []; 
-//         var elts = document.getElementsByClassName("graphs");
-//     });    
-
-
-
-// });
-// casper.run();*/
 
 
 
