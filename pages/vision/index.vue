cfdd843f34ccf173b0ad26c802305d58173403d2
Osama Saleh <osama.saleh@mail.utoronto.ca>
2020-08-15 16:21:36 -0400

    Vision page completed

---
+++ pages/vision/index.vue
@@ -0,0 +1,205 @@
+<template>
+
+  <div class="pb-5">
+    <div class="container g-hero mt-5">
+      <div class="row">
+        <div class="col-md-8">
+          <h1 class="g-hero-heading">
+            Vision
+          </h1>
+          <p
+            class="g-hero-tag"
+          > Promoting software liberty and excellence by developing FLOSS (Free/Libre Open Source Software) 
+          and educating students in FLOSS engineering.</p>
+          
+        </div>
+      </div>
+    </div>
+    <section class="container mt-5 pl-4 pb-5">
+      
+      <h1 class="g-section-heading mt-5">
+            Project Details
+      </h1>
+        
+      <div class="row mt-5">
+
+      <h1 class="g-section-subheading mt-5">
+            What's new about this ?
+      </h1>
+        
+        <p class="g-section-body mt-4 pl-4">Since writing and deploying functioning client facing software requires dedicated teams 
+        of skilled developers maintainers, open-source isn't always financially or operationally viable. We hope to address this by 
+        leveraging the curiosity, creativity, and time that students have.
+        </p>
+
+        
+        
+
+      </div>
+
+      
+      <div class="row mt-5">
+
+        <h1 class="g-section-subheading mt-5">
+                Our Software
+        </h1>
+
+        <div class="justify-content-center">
+            <p class="g-section-body mt-4 pl-4">
+            We aim to improve the state of the FLOSS ecosystem by helping develop and maintain useful, 
+            creative apps and dev-tools with the latest technology.
+            </p>
+            <div class="row d-flex pt-5 justify-content-center align-items-center mb-5">
+
+                <div class="mx-4">
+                <img
+                    class="icon-ide"
+                    src="@/assets/icons/vue-logo.png"
+                />
+                </div>
+
+                <div class="flex-col align-items-center mx-4">
+                <img
+                    class="icon-ide"
+                    src="@/assets/icons/android-studio-logo.png"
+                />
+                </div>
+
+                <div class="flex-col align-items-center mx-4">
+                <img
+                    class="icon-ide"
+                    src="@/assets/icons/flutter-logo.png"
+                />
+                
+                </div>
+                
+            </div>
+        </div>
+
+        <h1 class="g-section-subheading mt-5">
+                Education
+        </h1>
+
+        <p class="g-section-body mt-4 pl-4">
+        We aim to revolutionize the way students learn software engineering by empowering students to build useful 
+        and creative apps for human welfare with a mentor in an open-source environment. Students can join as an apprentice or an explorer.
+        </p>
+
+      
+
+        <p class="g-section-body mt-4 pl-4">
+        Apprentices want to get better at software engineering by contributing to useful 
+        software in a professional, open-source environment.
+        </p>
+
+        <p class="g-section-body mt-4 pl-4">
+        Grey Software maintains several open-source apps that apprentices can contribute to. 
+        Contributing to apps that are relevant in the real world, students can learn how the apps they interact with are built, 
+        deployed, and maintained.
+
+
+        </p>
+
+        
+        
+
+        <div class="justify-content-center">
+            <h1 class="g-section-subheading mt-5">
+                    How it works
+            </h1>
+
+
+            <p class="g-section-body mt-4 pl-4">
+            Explorers have an idea and would like to build a software prototype with a mentor 
+            that can guide them using best practices from the open-source software community.
+
+            </p>
+
+            
+            <p class="g-section-body mt-4 pl-4">
+            With their mentor’s guidance, explorers can scope their idea down into a viable 
+            software prototype and establish a remote, agile and async development environment 
+            with the help of tools such as Discord, VS Code, and Github.
+                
+            </p>
+            <div class="row d-flex pt-5 justify-content-center align-items-center">
+
+                <div class="mx-4">
+                <img
+                    class="icon-ide"
+                    src="@/assets/icons/discord-logo.png"
+                />
+                </div>
+
+                <div class="flex-col align-items-center mx-4">
+                <img
+                    class="icon-ide"
+                    src="@/assets/icons/vscode-logo.png"
+                />
+                </div>
+
+                <div class="flex-col align-items-center mx-4">
+                <img
+                    class="icon-ide"
+                    src="@/assets/icons/github-logo.png"
+                />
+                </div>
+                
+            </div> 
+        </div>   
+
+
+
+        
+
+        
+        
+
+      </div>
+      
+      
+        
+    </section>
+
+
+  </div>
+
+
+
+
+
+</template>
+
+
+<style scoped>
+
+
+.icon-ide{
+    height: 140px;
+
+
+
+}
+
+.first-image-element{
+    padding-left: 370px;
+    justify-content: center;
+    align-items: center;
+
+
+
+
+}
+
+.bottom-first-image-element{
+    padding-left: 320px;
+    justify-content: center;
+    align-items: center;
+
+    
+}
+
+
+
+
+</style>>
