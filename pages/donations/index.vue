f03e40f63c916f521462197e1f434c9224d5b29b
Osama Saleh <osama.saleh@mail.utoronto.ca>
2020-08-15 12:35:22 -0400

    Finished why-donate page

---
+++ pages/donations/index.vue
@@ -0,0 +1,74 @@
+<template>
+
+  <div class="pb-5">
+    <div class="container g-hero mt-5">
+      <div class="row">
+        <div class="col-md-8">
+          <h1 class="g-hero-heading">
+            Why Should I Donate ?
+          </h1>
+          <p
+            class="g-hero-tag"
+          > In order to honor its mission, Grey Software is a not-for-profit organization. 
+          The organization was founded so that we orient ourselves towards creating a better world instead of richer shareholders.</p>
+          
+        </div>
+      </div>
+    </div>
+    <section class="container mt-5 pl-4 pb-5">
+      <h1 class="g-section-subheading">
+        To Protect Your Freedoms
+      </h1>
+      <div class="row mt-5">
+        <p class="g-section-body mt-4 pl-4">"As our society grows more dependent on computers, 
+            the software we run is of critical importance to securing the future of a free society."
+        </p>
+
+        <p class="g-section-body mt-4 pl-4">With our lives increasingly governed by software written by corporations 
+        that monetize our data, it is more important than ever to protect our freedom to 
+        know what the software programs we run are doing.
+        </p>
+
+        <p class="g-section-body mt-4 pl-4">Grey Software creates open source software that anyone 
+        else can study and improve by looking into the source code. It holds us as an organization 
+        accountable for being ethical with the trust our users put in us.
+        </p>
+
+        <p class="g-section-body mt-4 pl-4">By donating, you help keep alive the dream of a future where humans 
+        use open source software that protects their freedoms.
+        </p>
+
+        
+
+
+
+      </div>
+      <h1 class="g-section-subheading mt-5">
+            To Revolutionize Software Education
+      </h1>
+      <div class="row mt-5">
+        
+        <p class="g-section-body mt-4 pl-4">The software education ecosystem we’re building encourages new, enthusiastic 
+        software developers to apprentice under maintainers on open source projects. This allows students to gain valuable experience 
+        while maintainers get enthusiastic developers and a source of income. We believe that a collaborative, education-driven environment 
+        such as this can advance software technology in a positive direction. By donating, you help fund the technical infrastructure needed 
+        to run our education programs, and provide the possibility of financial aid to students.
+        </p>
+
+        
+        
+
+      </div>
+      
+        
+    </section>
+
+
+  </div>
+
+
+
+
+
+</template>
+    
