502c3ad0d0431f9fa4ae47d8c09337ca9dff7776
mills <milindvishnoi@gmail.com>
2020-08-24 18:15:51 +0530

    apprentice-page complete

--- pages/apprentice/index.vue
+++ pages/apprentice/index.vue
@@ -6,7 +6,7 @@
         <div class="col-md-8">
           <h1 class="g-hero-heading">
             Apprentice Program
-          <br />
+            <br />
           </h1>
           <p
             class="g-hero-tag"
@@ -19,10 +19,58 @@
       </div>
     </div>
     <div class="container">
-      <h1 class="g-hero-heading">
-        The Problem
+      <!-- Problems -->
+      <h1 class="mt-5 mb-3 g-hero-heading">The Problem</h1>
+      <Rc1X3 :children="problems"></Rc1X3>
+      <div class="row mt-5">
+        <div class="col" style="padding-right: 20px;text-align:right;">
+          <h1
+            class="g-text-testimonial mt-5"
+          >“Without having the assistance whenever we need it, we had to spend hours googling and asking online.”</h1>
+          <p class="g-section-body mt-4" style="max-width: 100%;">
+            - Baichen
+          </p>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col" style="padding-left: 20px">
+          <h1
+            style="max-width: 84%;"
+            class="g-text-testimonial mt-5"
+          >“Even though tons of youtube videos are available, it’s a different experience to have someone that understands what you are going through to guide you in the project.”</h1>
+          <p class="g-section-body mt-4">
+            - Shawn
+          </p>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col" style="padding-right: 20px;text-align:right;">
+          <h1
+            class="g-text-testimonial mt-5"
+          >“As an individual project, you are required to know every aspect of the component including frontend, backend, etc. As a result of that, for most students...they have a significant knowledge gap to fill. This preparation process can be time-consuming and discouraging.”</h1>
+          <p class="g-section-body mt-4" style="max-width: 100%;">
+            - Brian
+          </p>
+        </div>
+      </div>
+
+      <!-- Solutions -->
+      <h1 class="mt-5 mb-3 g-hero-heading">Our Solution</h1>
+      <p
+        class="g-hero-tag"
+      >Grey Software developed the apprentice program as an internship alternative to student software developers. You'll collaborate on open source software with structure and guidance from a mentor, and walk away with valuable skills and a public demonstration of your work that you can take anywhere.</p>
+      <div class="g-hero-cta-container mb-3">
+        <a href="/projects">
+          <b-button size="lg" class="mr-4 g-btn-cta">Our Apprentice Projects</b-button>
+        </a>
+      </div>
+      <Rc1X3 :children="solutions"></Rc1X3>
+
+      <!-- Features -->
+      <h1 class="mt-5 mb-3 g-hero-heading">
+        Features
       </h1>
-      <Rc1X3 :children="children"></Rc1X3>
+      <Rc2X3 :children="features"></Rc2X3>
     </div>
   </div>
 </template>
@@ -31,25 +79,73 @@
 export default {
   data() {
     return {
-      children : [
+      problems: [
         {
-          desc: "Formal education doesn't to prepare you for the world's software ecosystem.",
-          icon: "../assets/icons/educate.svg"
+          desc:
+            "Formal education doesn't to prepare you for the world's software ecosystem.",
+          icon: "/apprentice/training.svg",
         },
         {
-          desc: "Students crave real-world software development experience, because it plays an important role when they’re recruited into the modern tech economy.",
-          icon: "../assets/icons/educate.svg"
+          desc:
+            "Students crave real-world software development experience, because it plays an important role when they’re recruited into the modern tech economy.",
+          icon: "/apprentice/coding.svg",
         },
         {
-          desc: "They also need guidance and structure as they gain this experience.",
-          icon: "../assets/icons/educate.svg"
+          desc:
+            "They also need guidance and structure as they gain this experience.",
+          icon: "/apprentice/mentor.svg",
+        },
+      ],
+      solutions: [
+        {
+            title: "Contribute to Production Software",
+            desc: "You'll work on open source apps that Grey Software deploys, and that users interact with.",
+            icon: "/apprentice/software-development.svg"
+        },
+        {
+            title: "Access to a mentor",
+            desc: "Your mentor's goal is to help you grow as an open source software developer. They help you by attending live collaboration sessions, answering questions, reviewing code, and structuring your learning experience.",
+            icon: "/apprentice/teacher.svg"
+        },
+        {
+            title: "Collaborative and Educational",
+            desc: "In addition to a mentor, you'll collaborate with other students working on the same project(s) as you.",
+            icon: "/apprentice/programmer.svg"
         }
-      ]
-    }
-  }
-}
+      ],
+      features: [
+          {
+            title: "Cutting Edge Tech",
+            desc: "Grey Software works with the technologies of the future. We'll match you with a project that lets you use the tools you're proficient or interested in.",
+            icon: "/apprentice/technology-products.svg"
+          },
+          {
+            title: "More than Code",
+            desc: "You'll get a chance to explore your potential as a designer or product owner, and gain a well-rounded set of skills.",
+            icon: "/apprentice/code.svg"
+          },
+          {
+            title: "Designed to be remote",
+            desc: "Founded during the COVID-19 pandemic, the apprentice program was designed to be remote. Our first 3 students were learning from Canada, India, and Indonesia!",
+            icon: "/apprentice/global.svg"
+          },
+          {
+            title: "Financially Empthetic",
+            desc: "We're considerate of your financial background, and are open to adjusting our rates or creating customized programs that require you to give back as a mentor after you graduate.",
+            icon: "/apprentice/loan.svg"
+          },
+          {
+            title: "Made for learning",
+            desc: "The apprentice program was designed to help budding software developers learn by contributing to software that matters, so your education always comes before the project.",
+            icon: "/apprentice/online-course.svg"
+          },
+          {
+            title: "Public Portfolio Upgrade",
+            desc: "You'll graduate from the program with a software project(s) to put on your portfolio, and a public log of your Github contributions for employers to look at.",
+            icon: "/apprentice/portfolio.svg"
+          },
+        ]
+    };
+  },
+};
 </script>
-
-<style>
-
-</style>
