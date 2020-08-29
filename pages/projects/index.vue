518ec5f2c2df771058be2ab9ce263e1bafa26b44
ArsalaBangash <arsalabangash@gmail.com>
2020-08-29 02:45:57 -0400

    Revamped project cards

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -9,15 +9,6 @@
             href="https://www.github.com/grey-software"
             target="_blank"
           >
-            <b-button
-              size="lg"
-              class="g-btn-cta-alt"
-            >
-              <fa-icon
-                class="mr-2"
-                :icon="['fab', 'github']"
-              ></fa-icon>Github
-            </b-button>
           </a>
         </div>
         <div class="col-lg-4 d-flex flex-column mt-3">
@@ -28,147 +19,25 @@
 
         </div>
       </div>
-    </div>
-    <div class="container px-0 pb-5">
-      <div class="justify-content-space-between align-items-center">
-        <div class="col-lg mb-5">
-          <h1 class="g-section-heading">Toonin</h1>
-          <div class="d-flex align-items-center g-section-info">Status: <span
-              style="background-color:#ffcc00;"
-              class="status-dot"
-            ></span>Live Beta</div>
-          <p class="g-section-body mt-4">Technology that allows you to tune in to your friends and family in realtime using peer to peer sharing.</p>
-          <div class="d-flex mt-5">
-            <b-button
-              size="lg"
-              class="g-btn mr-4"
-              target="_blank"
-              href="https://www.toonin.ml"
-            >
-              <fa-icon
-                class="mr-2"
-                :icon="['fas', 'play']"
-              ></fa-icon>Live Demo
-            </b-button>
-            <b-button
-              size="lg"
-              class="g-btn-alt"
-              target="_blank"
-              href="https://github.com/grey-software/material-math"
-            >
-              <fa-icon
-                class="mr-2"
-                :icon="['fab', 'github']"
-              ></fa-icon>Repository
-            </b-button>
-          </div>
-        </div>
-        <div class="col-lg">
-          <div class="embed-responsive embed-responsive-16by9">
-            <iframe
-              class="embed-responsive-item"
-              src="https://www.youtube.com/embed/ImxxRMPVHzs"
-              frameborder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            ></iframe>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="container px-0 mt-5">
-      <div class="justify-content-space-between align-items-center">
-        <div class="col-lg mb-5">
-          <h1 class="g-section-heading">Material Math</h1>
-          <div class="d-flex align-items-center g-section-info">Status: <span
-              style="background-color:#FF3366;"
-              class="status-dot"
-            ></span>Live Alpha</div>
-          <p class="g-section-body mt-4">The spiritial successor to Boltz on Android, Material Math brings unlimited mental math practice in a fun, beautiful interface to the web!</p>
-          <div class="d-flex mt-5">
-            <b-button
-              size="lg"
-              class="g-btn mr-4"
-              href="https://material-math.netlify.app"
-              target="_blank"
-            >
-              <fa-icon
-                class="mr-2"
-                :icon="['fas', 'play']"
-              ></fa-icon>Live Demo
-            </b-button>
-            <b-button
-              size="lg"
-              class="g-btn-alt"
-              href="https://github.com/grey-software/material-math"
-              target="_blank"
-            >
-              <fa-icon
-                class="mr-2"
-                :icon="['fab', 'github']"
-              ></fa-icon>Repository
-            </b-button>
-          </div>
-        </div>
-        <div class="col-lg">
-          <div class="embed-responsive embed-responsive-16by9">
-            <iframe
-              class="embed-responsive-item"
-              src="https://www.youtube.com/embed/ceACiAdXSDc"
-              frameborder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            ></iframe>
-          </div>
-        </div>
-      </div>
-    </div>
-          <div class="container px-0 mt-5">
-        <div class="justify-content-space-between align-items-center">
 
-          <div class="col-lg mb-5">
-            <h1 class="g-section-heading">LinkedInFocus</h1>
-            <div class="d-flex align-items-center g-section-info">Status: <span
-                style="background-color:#ffcc00;"
-                class="status-dot"
-              ></span>Unofficial Release</div>
-            <p class="g-section-body mt-4">A web extension that allows you to hide the news feed and news panel on LinkedIn so you can focus on your career!</p>
-            <div class="d-flex mt-5">
-              <b-button
-                size="lg"
-                class="g-btn mr-4"
-                href="https://github.com/grey-software/LinkedInFocus/releases/tag/0.01"
-                target="_blank"
-              >
-                <fa-icon
-                  class="mr-2"
-                  :icon="['fas', 'box-open']"
-                ></fa-icon>Latest Release
-              </b-button>
-              <b-button
-                size="lg"
-                class="g-btn-alt"
-                href="https://github.com/grey-software/material-math"
-                target="_blank"
-              >
-                <fa-icon
-                  class="mr-2"
-                  :icon="['fab', 'github']"
-                ></fa-icon>Repository
-              </b-button>
-            </div>
-          </div>
-          <div class="col-lg">
-            <img style="width: 100%;" src="https://github.com/grey-software/LinkedInFocus/raw/master/after.png" />
-          </div>
+      <section class="container px-0">
+        <div class="project-cards-container">
+
+          <project-card
+            v-for="(project,id) in projects"
+            :key="id"
+            :project="project"
+          />
         </div>
 
-        
-      </div>
+      </section>
+    </div>
+
   </div>
 </template>
 
 <script>
+import ProjectCard from "../../components/ProjectCard.vue";
 export default {
   data () {
     return {
@@ -176,66 +45,61 @@ export default {
         {
           name: "Toonin",
           desc:
-            "Toonin allows you to share or stream audio and video over the web in real time.",
-          githubLink: "https://github.com/grey-software/toonin",
-          icon: "/projects/toonin/logo.svg",
+            "Share or stream audio and video over the web in real time.",
+          repo: "https://github.com/grey-software/toonin",
+          demo: "https://www.toonin.ml",
+          logo: "/projects/toonin/logo.svg",
           link: "/projects/toonin",
-          starCount: 54
+          status: "Live Beta"
         },
         {
           name: "Material Math",
           desc:
-            "Material Math allows you to practice math problems in a fun, beautiful, material experience!",
-          githubLink: "https://github.com/grey-software/material-math",
-          icon: "/projects/material-math/logo.svg",
+            "Practice math problems in a fun, beautiful, material experience!",
+          repo: "https://github.com/grey-software/material-math",
+          demo: "https://material-math.grey.software",
+          logo: "/projects/material-math/logo.png",
           link: "/projects/material-math",
-          starCount: 3
+          status: "Live Alpha",
         },
         {
-          name: "Open Mind",
+          name: "Twitter Focus",
           desc:
-            "Open Mind allows you to organize your learning and research using mind maps!",
-          icon: "/projects/open-mind/logo.svg",
-          githubLink: "https://github.com/grey-software/openmind",
-          link: "projects/open-mind",
-          starCount: 4
-        },
-        {
-          name: "LinkedInFocus",
+            "Hide the feed and What's Happening panel on Twitter so you can focus and control your experience!",
+          logo: "/projects/twitter-focus/logo.png",
+          repo: "https://github.com/grey-software/Twitter-Focus",
+          demo: "https://chrome.google.com/webstore/detail/twitter-focus/kmdpomipbibobgdgfeidajmnlecloeml?hl=en&authuser=2",
+          link: "/projects/twitter-focus",
+          status: "Live Beta"
+        }, {
+          name: "LinkedIn Focus",
           desc:
-            "LinkedInFocus allows you to hide the news feed and news panel on LinkedIn so you can focus on your career!",
-          icon: "/projects/linkedin-focus/logo.png",
-          githubLink: "https://github.com/grey-software/LinkedIn-InFocus",
+            "Hide the news feed and news panel on LinkedIn so you can focus on your career!",
+          logo: "/projects/linkedin-focus/logo.png",
+          repo: "https://github.com/grey-software/LinkedIn-Focus",
+          demo: "https://chrome.google.com/webstore/detail/linkedin-focus/cmafljjdkloacahjddlpaognhjpacdff?hl=en&authuser=2",
           link: "/projects/linkedin-focus",
-          starCount: 1
-        }
-      ]
+          status: "Live Beta"
+        },
+      ],
     };
-  }
+  },
+  components: {
+    ProjectCard,
+  },
 };
 </script>
 
 <style>
-.status-dot {
-  height: 28px;
-  width: 28px;
-  border-radius: 50%;
-  display: inline-block;
-  transition: opacity 0.2s ease-out;
-  margin: 0px 8px 0px 12px;
-}
-.status-dot:hover {
-  opacity: 0.8;
-}
-
 .iframe-container {
   overflow: hidden;
   padding-top: 56.25%;
   position: relative;
 }
 
-/* .project-promo-video {
-  max-width: 560px;
-  max-height: 315px;
-} */
+.project-cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
 </style>
