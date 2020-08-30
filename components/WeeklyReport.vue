c21662e5a7cedd2068b28b3784d4ca01f454f5a7
osama hatem <osamahatem@osamas-MacBook-Pro.local>
2020-08-29 23:33:04 -0400

    Weekly Report Card and Scraping Tool Completed

---
+++ components/WeeklyReport.vue
@@ -0,0 +1,272 @@
+<template>
+    <div class="d-flex container mt-5">
+        <div class="d-flex overview-header">
+            <h5 class="table-header ml-3">Overview</h5>
+            <h5 class="table-date ml-3">{{startDate}} - {{endDate}}</h5>
+        
+        
+        
+        </div>
+
+
+        <div class="d-flex pr-section">
+            <div class="d-flex detail-box">
+                <div class="d-flex img-container">
+                    <img src="../assets/icons/merge-pr.svg" class="mb-2 mr-1"/>
+                    <h4 class="mb-1">{{mergePR}}</h4>
+                </div>
+                <p>Merged Pull Requests</p>
+
+            </div>   
+
+            <div class="d-flex detail-box">
+                <div class="d-flex img-container">
+                    <img src="../assets/icons/merge-open-pr.svg" class="mb-2 mr-1"/>
+                    <h4 class="mb-1">{{mergeOpenPR}}</h4>
+                </div>
+                <p>Open Pull Requests</p>
+
+            </div> 
+
+            <div class="d-flex detail-box">
+                <div class="d-flex img-container">
+                    <img src="../assets/icons/closed-issues.svg" class="mb-2 mr-1"/>
+                    <h4 class="mb-1">{{closedIssues}}</h4>
+                </div>
+                <p>Closed Issues</p>
+            </div> 
+
+            <div class="d-flex detail-box">
+                <div class="d-flex img-container">
+                    <img src="../assets/icons/new-issues.svg" class="mb-2 mr-1"/>
+                    <h4 class="mb-1">{{newIssues}}</h4>
+                </div>
+                <p>New Issues</p>
+            </div>      
+        
+        
+        </div>
+
+
+        <div class="d-flex branches-section">
+            <div class="d-flex branches-title">
+                <div class="d-flex title">
+                    <h5 class="mt-2 mb-2">Master</h5>
+                
+                
+                </div>
+
+            
+            </div>
+
+            <div class="d-flex branches-info">
+
+                <div class="d-flex detail-box">
+                    <h5 class="mt-2 mb-2">{{authors}} Authors</h5>
+
+                </div>   
+
+                <div class="d-flex detail-box">
+                    <h5 class="mt-2 mb-2">{{commitsToMaster}} Commits</h5>
+
+                </div> 
+
+                <div class="d-flex detail-box">
+                    <h5 class="mt-2 mb-2">{{totalAdditions}} Additions</h5>
+                </div> 
+
+                <div class="d-flex detail-box">
+                    <h5 class="mt-2 mb-2">{{deletions}} Deletions</h5>
+                </div> 
+            
+            </div>
+        
+        
+        </div>
+    
+    
+    
+    </div>
+
+
+
+
+    
+</template>
+
+
+<script>
+export default {
+    props: {
+        mergePR: {
+            type: String,
+            default: "0"
+        },
+
+        mergePR: {
+            type: String,
+            default: "0"
+        },
+
+        mergeOpenPR: {
+            type: String,
+            default: "0"
+        },
+
+        newIssues: {
+            type: String,
+            default: "0"
+        },
+
+        closedIssues: {
+            type: String,
+            default: "0"
+        },
+
+        commitsToMaster: {
+            type: String,
+            default: "0"
+        },
+
+        totalAdditions: {
+            type: String,
+            default: "0"
+        },
+
+        deletions: {
+            type: String,
+            default: "0"
+        },
+
+        authors: {
+            type: String,
+            default: "0"
+        },
+
+        startDate: {
+            type: String,
+            default: "August 22, 2020"
+        },
+
+        endDate: {
+            type: String,
+            default: "August 29, 2020"
+        },
+        
+    }
+
+
+
+}
+</script>
+
+
+<style scoped>
+
+
+
+.container{
+    flex-direction: column;
+    border-color: grey;
+
+
+
+
+}
+
+.overview-header{
+    background-color: grey;
+    flex: 1;
+    border-color: grey;
+    border-width: 2px;
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+    height: 40px;
+    align-items: center;
+    flex-direction: row;
+
+
+
+
+}
+
+.table-header{
+    color: black;
+    flex: 7
+
+
+}
+
+.table-date{
+    color: black;
+    flex: 2;
+    font-style: italic;
+
+
+}
+
+.pr-section{
+    flex: 3;
+    flex-direction: row;
+    border-color: grey;
+    height: 80px;
+    
+
+
+
+}
+
+.detail-box{
+    flex: 1;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-width: 2px;
+    border-color: grey;
+    height: 100%;
+
+
+}
+
+.img-container{
+    flex-direction: row;
+
+
+}
+
+
+.branches-section{
+    flex: 3;
+    flex-direction: column;
+    border-color: grey;
+
+
+
+}
+
+.branches-title{
+    flex-direction: row;
+    
+
+}
+
+.title{
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    border-color: grey;
+    border-width: 2px
+    
+
+
+
+}
+
+div.imgicon {
+    filter: brightness(0) invert(1);
+}
+
+
+
+
+</style>
---
+++ weekly-report/WeeklyScrape.js
@@ -0,0 +1,131 @@
+const { _ } = require('core-js');
+
+
+
+getWeeklyInsights = async (link, repos) => {
+    const cheerio = require('cheerio');
+    const puppeteer = require('puppeteer');
+    const fs = require('fs');
+    const url = link;
+    const repo = link.split('/')[4]
+  
+    const start = new Date()
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+  
+    await page.goto(url, { waitUntil: 'networkidle0' });
+  
+  
+    await page.waitForSelector('.octicon-git-merge', {
+      visible: true,
+    })
+  
+    const pageContent = await page.content()
+  
+    const $ = cheerio.load(pageContent);
+    var commitsToMaster = "";
+    var authors = "";
+    var files = "";
+    var totalAdditions = "";
+    var deletions = "";
+    const startDate = $('.Subhead-heading')['0'].children[0].data.trim('\n');
+    const endDate = $('.dash')['0'].next.data.trim('\n');
+
+    //Scraping for img link  
+
+    $('.text-gray').each(function (i, elem) {
+        if($(elem).find('strong')['0'] != undefined){
+            commitsToMaster = $(elem).find('strong').find('span')['0'].children[0].data
+            authors = $(elem).find('strong')['0'].children[0].data
+            files = $(elem).find('strong')['3'].children[0].data
+            totalAdditions = $(elem).find('strong')['4'].children[0].data
+            deletions = $(elem).find('strong')['6'].children[0].data
+        }
+
+
+    });
+
+    
+    $('.Box-row').each(function (i, elem) {
+        if($(elem).find('ul')[0] != undefined){
+            //console.log($(elem).find('ul').find('span')[0].children[0].data)
+            /*console.log($(elem).find('ul').find('span')[2].children[0].data);
+            console.log($(elem).find('ul').find('span')[4].children[0].data);
+            console.log($(elem).find('ul').find('span')[6].children[0].data);*/
+
+            const mergePR = $(elem).find('ul').find('svg')['0'].next.data.trim('\n', ' ')
+            const mergeOpenPR = $(elem).find('ul').find('svg')['1'].next.data.trim('\n', ' ')
+            const newIssues =  $(elem).find('ul').find('svg')['2'].next.data.trim('\n', ' ')
+            const closedIssues = $(elem).find('ul').find('svg')['3'].next.data.trim('\n', ' ')
+
+            repos.push({
+                repo: repo,
+                mergePR: mergePR,
+                mergeOpenPR: mergeOpenPR,
+                newIssues: newIssues,
+                closedIssues: closedIssues,
+                commitsToMaster: commitsToMaster,
+                authors: authors,
+                files: files,
+                totalAdditions: totalAdditions,
+                deletions: deletions,
+                startDate: startDate,
+                endDate: endDate
+
+
+            
+            });
+
+            
+            
+        }
+        
+
+
+      //const avatar = $(elem).find('img')[0].attribs.src
+  
+  
+    });
+  
+  
+    var jsonContent = JSON.stringify(repos);
+  
+    fs.writeFile("repo-reports.json", jsonContent, 'utf8', function (err) {
+      if (err) {
+          console.log("An error occured while writing JSON Object to File.");
+          return console.log(err);
+      }
+   
+    });
+   
+  
+  
+    
+  
+    await browser.close();
+    const end = new Date() - start
+  };
+  
+  var links = [
+      'https://github.com/grey-software/toonin/pulse',
+      'https://github.com/grey-software/LinkedInFocus/pulse',
+
+  ];
+
+  const repos = [];
+
+  module.exports = { getWeeklyInsights }
+  
+  for(const link of links){
+    getWeeklyInsights(link, repos);
+
+
+
+
+  }
+  
+  
+  
+  
+  
+  
