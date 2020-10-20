6ca190a6be15a58cc02cc5f8302f8e71db0dd64f
ArsalaBangash <arsalabangash@gmail.com>
2020-10-20 23:12:45 +0500

    Added hacktoberfest page

--- components/CenteredHero.vue
+++ components/CenteredHero.vue
@@ -3,42 +3,15 @@
     <div class="row d-flex justify-center">
       <img :src="icon" alt="Grey Software's Icon" class="hero-img" />
     </div>
-    <div class="mt-3 mt-md-4 mt-lg-5 row d-flex flex-column align-items-center">
+    <div class="mt-3 mt-md-4 mt-lg-5 flex flex-column align-items-center">
       <div class="col-lg-10 text-center">
         <h1 class="hero-heading">{{ title }}</h1>
         <p class="hero-tag">{{ desc }}</p>
       </div>
     </div>
-    <div class="mt-3 d-flex justify-content-center">
-      <b-button 
-        v-if="button1 && button1.link && button1.label" 
-        :href="button1.link" 
-        :size="gtmd" 
-        class="mr-4 btn-cta mb-3 d-flex align-items-center">
-          <img
-            v-if="button1.icon"
-            class="btn-icon mx-2"
-            :src="button1.icon"
-          />
-          {{ button1.label }}
-      </b-button>
-      <b-button 
-        v-if="button2 && button2.link && button2.label" 
-        :href="button2.link" 
-        :size="gtmd" 
-        variant="outline-primary" 
-        class="btn-cta-alt mb-3 d-flex align-items-center">
-          <img
-            v-if="button2.icon"
-            class="btn-icon mx-2"
-            :src="button2.icon"
-          />
-          {{ button2.label }}
-      </b-button>
-    </div>
-    <div class="row d-flex justify-center">
-      <img id="arrow" src="/downarrow.png" alt="Down Arrow" />
-    </div>
+    <a href="#projects" class="row d-flex justify-center">
+        <img class="bounce" id="arrow" src="/icons/arrow.svg" alt="Down Arrow"
+      /></a>
   </div>
 </template>
 
@@ -48,42 +21,42 @@ export default {
     icon: String,
     desc: String,
     title: String,
-    button1:  {
+    button1: {
       label: {
         type: String,
-        default: ''
+        default: "",
       },
       link: {
         type: String,
-        default: ''
+        default: "",
       },
       icon: {
         type: String,
-        default: ''
-      }
+        default: "",
+      },
     },
     button2: {
       label: {
         type: String,
-        default: ''
+        default: "",
       },
       link: {
         type: String,
-        default: ''
+        default: "",
       },
       icon: {
         type: String,
-        default: ''
-      }
-    }
+        default: "",
+      },
+    },
   },
   computed: {
     gtmd() {
-      if (this.$mq === 'sm' || this.$mq === 'xs' || this.$mq === 'md')
-        return 'md';
-      return 'lg';
-    }
-  }
+      if (this.$mq === "sm" || this.$mq === "xs" || this.$mq === "md")
+        return "md";
+      return "lg";
+    },
+  },
 };
 </script>
 
@@ -154,6 +127,33 @@ export default {
   .hero-img {
     width: 100px;
   }
+  #arrow {
+    width: 50px;
+    bottom: 25px;
+  }
+}
+
+.bounce {
+  -webkit-animation: bounce 0.75s infinite;
+}
+
+@-webkit-keyframes bounce {
+  25% {
+    transform: scale(1, 0.9) translate(0, 0);
+  }
+
+  75% {
+    transform: scale(1, 1) translate(0, -10px);
+  }
+}
+
+@media screen and (max-width: 992px) {
+  #arrow {
+    width: 60px;
+  }
+}
+
+@media (max-width: 576px) {
   #arrow {
     width: 50px;
     bottom: 25px;
